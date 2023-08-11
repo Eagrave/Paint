e422e9c9b568e3f5c14fb6e09874bf50a6c72801
Eagrave <agrave.elisar@gmail.com>
2023-08-11 21:35:06 +0800

    added color picker

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 const parentBox = document.querySelector(".grid-container");
 const clearButton = document.querySelector(".button-clear")
 let isMouseDown = false;
+const pickr = Pickr.create({
+    el: '.color-picker',
+    theme: 'nano', 
+
+    components: {
+
+     
+        preview: true,
+        opacity: true,
+        hue: true,
+
+       
+        interaction: {
+            hex: true,
+            rgba: false,
+            hsla: false,
+            hsva: false,
+            cmyk: false,
+            input: false,
+            clear: false,
+            save: true
+        }
+    }
+});
+
+
 const createGrid = ()=>{
+let hex = ''
+ pickr.on('save', (color, instance) => {
+    let newColor = color.toHEXA();
+    hex = '#' + newColor.join('');
+  
+});
     for(let i=0; i<256; i++){
         const newBox = document.createElement("div");
         newBox.className = `grid-item`;
         parentBox.appendChild(newBox);
         newBox.addEventListener('mousedown', (e) => {
             isMouseDown = true;
-            e.target.style.backgroundColor = 'black';
+            e.target.style.backgroundColor = hex;
         })
         newBox.addEventListener('mouseup', () => {
             isMouseDown = false;
@@ -16,14 +48,17 @@ const createGrid = ()=>{
         })
         newBox.addEventListener('mousemove', (e) => {
             if(isMouseDown) {
-                e.target.style.backgroundColor = 'black';
+                e.target.style.backgroundColor = hex;
             }
         });
 
     }
+
 }
 
 createGrid();
+
+
 clearButton.addEventListener('click',()=> {
     let boxes = document.querySelectorAll(".grid-item");
     boxes.forEach((box) => {
