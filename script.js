b39e39e90a887279cc958e4019948655b2de155e
Eagrave <agrave.elisar@gmail.com>
2023-08-11 20:49:13 +0800

    added sketch and clear function

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const parentBox = document.querySelector(".grid-container");
+const clearButton = document.querySelector(".button-clear")
+let isMouseDown = false;
 const createGrid = ()=>{
     for(let i=0; i<256; i++){
         const newBox = document.createElement("div");
         newBox.className = `grid-item`;
         parentBox.appendChild(newBox);
+        newBox.addEventListener('mousedown', (e) => {
+            isMouseDown = true;
+            e.target.style.backgroundColor = 'black';
+        })
+        newBox.addEventListener('mouseup', () => {
+            isMouseDown = false;
+           
+        })
+        newBox.addEventListener('mousemove', (e) => {
+            if(isMouseDown) {
+                e.target.style.backgroundColor = 'black';
+            }
+        });
+
     }
 }
 
 createGrid();
+clearButton.addEventListener('click',()=> {
+    let boxes = document.querySelectorAll(".grid-item");
+    boxes.forEach((box) => {
+        box.style.backgroundColor="white";
+    })
+
+})
